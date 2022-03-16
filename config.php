feab5b4026404d91b3f2b685d96c58f7f5fe812d
georgekoutras <george.koutras@gmail.com>
2022-03-16 22:45:00 +0200

    Final open source commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'elearning';
+$CFG->dbuser    = 'openrobot';
+$CFG->dbpass    = '5$uT$t2N$NsP$hU$gs$7Bn';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://elearning.openrobot.eu';
+$CFG->dataroot  = '/home/openrobot/domains/elearning.openrobot.eu/moodle_data';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
